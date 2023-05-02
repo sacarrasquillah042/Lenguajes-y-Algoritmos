df8764900fa0278d940af847c62e50aad534802b
campereza043 <129222981+campereza043@users.noreply.github.com>
2023-05-01 22:04:12 -0500

    Update C++.cpp

--- CPerezA/Taller/C++.cpp
+++ CPerezA/Taller/C++.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Ingrese la cantidad de veces que quiere que 'Hola mundo' aparezcan ";
+    cout << "Ingrese la cantidad de veces que 'Hola mundo' aparezcan ";
     cin >> n;
     for (int i = 0; i < n; i++) {
         cout << "Hola mundo ";
